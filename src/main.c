0aa972b3be98e908efb2bf8f6c84f0f28b562e48
AndresFC1309 <afcuartasa@unal.edu.co>
2020-03-31 13:27:50 -0500

    Cambios

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <stdio.h>#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
 
 void main(){
     print("hello world");
     return 0;
+}
+
+void main ()
+{
+  float numero,seno,coseno,tangente;
+  clrscr();
+  numero=2*Pi;
+  seno=sin(numero);
+  coseno=cos(numero);
+  tangente=tan(numero);
+  printf("Los valores son:\nSeno:%f\nCoseno:%f\nTangente:%f",seno,coseno,tangente);
+  getch();
 }
